5dcf9b7a507bc445c8f2119d78d67a88f82ae093
Lucas <lucas.christianson01@gmail.com>
2026-08-28 00:18:03 -0400

    Moved view to view directory.  Added textboxes, textblocks, and buttons to view.

--- TakeHomeAssignment/Views/MainWindow.xaml.cs
+++ TakeHomeAssignment/Views/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +29,11 @@ namespace TakeHomeAssignment
         public MainWindow()
         {
             InitializeComponent();
+
+            var windowId = Win32Interop.GetWindowIdFromWindow(WindowNative.GetWindowHandle(this));
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            appWindow.Resize(new Windows.Graphics.SizeInt32(400, 300));
         }
     }
 }
